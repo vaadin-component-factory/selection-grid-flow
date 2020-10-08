6d0e74be6e152ef44d27bff29282bbffc579b0ce
Milad Karbasizadeh <milad@vaadin.com>
2020-10-08 14:39:47 +0300

    feat: shift + select with mouse

--- selection-grid-flow-demo/frontend/src/selection-grid-connector.js
+++ selection-grid-flow-demo/frontend/src/selection-grid-connector.js
@@ -1,13 +1,29 @@
 const Grid = customElements.get('vaadin-grid');
 if (Grid) {
-    // store the original method
-    const selectedItemsChanged = Grid.prototype._selectedItemsChanged;
-    Grid.prototype._selectedItemsChanged = function  _selectedItemsChanged(e) {
-        console.log("test overload");
-        if (this.$.items.children.length && (e.path === 'selectedItems' || e.path === 'selectedItems.splices')) {
-            Array.from(this.$.items.children).forEach(row => {
-                this._updateItem(row, row._item);
-            });
+    const oldClickHandler = Grid.prototype._onClick;
+
+    Grid.prototype._onClick = function _click(e) {
+        const boundOldClickHandler = oldClickHandler.bind(this);
+        boundOldClickHandler(e);
+        const eventTarget = e.target;
+        if (e.shiftKey && eventTarget.nodeName.toLowerCase() === 'vaadin-checkbox') {
+            // the click happened on a checkbox
+            if (eventTarget.checked) {
+                const orderedSelectedKeys = this.selectedItems.map(i => parseInt(i.key)).sort((a, b) => a - b);
+                const selectedKey = this.selectedItems[this.selectedItems.length - 1].key;
+                const selectedKeyIndex = orderedSelectedKeys.indexOf(parseInt(selectedKey));
+                if (selectedKeyIndex > 0) {
+                    const previousItemKey = orderedSelectedKeys[selectedKeyIndex - 1];
+                    const selectedItemKey = parseInt(selectedKey);
+                    Array.from(this.$.items.children).forEach(row => {
+                        const rowItem = row._item;
+                        const itemKey = parseInt(row._item.key);
+                        if (itemKey < selectedItemKey && itemKey > previousItemKey) {
+                            this.selectItem(rowItem);
+                        }
+                    });
+                }
+            }
         }
-    };
+    }
 }
