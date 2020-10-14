967fab1743288c77ec57c780898b4b7b00246ea4
Milad Karbasizadeh <milad@vaadin.com>
2020-10-14 12:58:28 +0300

    --wip-- [skip ci]

--- selection-grid-flow/src/main/resources/META-INF/resources/frontend/src/selection-grid-connector.js
+++ selection-grid-flow/src/main/resources/META-INF/resources/frontend/src/selection-grid-connector.js
@@ -194,28 +194,54 @@ customElements.whenDefined("vaadin-grid").then(() => {
 
     const oldClickHandler = Grid.prototype._onClick;
     Grid.prototype._onClick = function _click(e) {
-        const boundOldClickHandler = oldClickHandler.bind(this);
-        boundOldClickHandler(e);
-        const eventTarget = e.target;
-        if (e.shiftKey && eventTarget.nodeName.toLowerCase() === 'vaadin-checkbox') {
-            // the click happened on a checkbox
-            if (eventTarget.checked) {
-                const orderedSelectedKeys = this.selectedItems.map(i => parseInt(i.key)).sort((a, b) => a - b);
-                const selectedKey = this.selectedItems[this.selectedItems.length - 1].key;
-                const selectedKeyIndex = orderedSelectedKeys.indexOf(parseInt(selectedKey));
-                if (selectedKeyIndex > 0) {
-                    const previousItemKey = orderedSelectedKeys[selectedKeyIndex - 1];
-                    const selectedItemKey = parseInt(selectedKey);
-                    Array.from(this.$.items.children).forEach(row => {
-                        const rowItem = row._item;
-                        const itemKey = parseInt(row._item.key);
-                        if (itemKey < selectedItemKey && itemKey > previousItemKey) {
-                            this.selectItem(rowItem);
-                        }
-                    });
+      const boundOldClickHandler = oldClickHandler.bind(this);
+      boundOldClickHandler(e);
+      const eventTarget = e.target;
+      if (
+        e.shiftKey &&
+        eventTarget.nodeName.toLowerCase() === "vaadin-checkbox"
+      ) {
+        // the click happened on a checkbox
+        if (eventTarget.checked) {
+          const orderedSelectedKeys = this.selectedItems
+            .map((i) => parseInt(i.key))
+            .sort((a, b) => a - b);
+          const selectedKey = this.selectedItems[this.selectedItems.length - 1]
+            .key;
+          const selectedKeyIndex = orderedSelectedKeys.indexOf(
+            parseInt(selectedKey)
+          );
+          if (selectedKeyIndex > 0) {
+            const previousItemKey = orderedSelectedKeys[selectedKeyIndex - 1];
+            const selectedItemKey = parseInt(selectedKey);
+
+            let maxInspected = previousItemKey;
+            let scrollTargetIndex = 0;
+            debugger
+            console.log('gonna select', previousItemKey, selectedItemKey)
+            while (maxInspected < selectedItemKey) {
+              this.scrollToIndex(maxInspected);
+              Array.from(this.$.items.children).forEach((row) => {
+                const rowKey = parseInt(row._item.key);
+                if (rowKey < selectedItemKey && rowKey > previousItemKey) {
+                  this.selectItem(row._item);
+                  scrollTargetIndex = row.index;
                 }
+              });
+              maxInspected = parseInt(Array.from(this.$.items.children).sort((a, b) => parseInt(a._item.key) - parseInt(b._item.key))[this.$.items.children.length - 1]._item.key);
             }
+            this.scrollToIndex(scrollTargetIndex);
+            // Array.from(this.$.items.children).forEach((row) => {
+            //   const rowItem = row._item;
+            //   console.log(rowItem);
+            //   const itemKey = parseInt(row._item.key);
+            //   if (itemKey < selectedItemKey && itemKey > previousItemKey) {
+            //     this.selectItem(rowItem);
+            //   }
+            // });
+          }
         }
-    }
+      }
+    };
   }
 });
