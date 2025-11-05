514925225216dec13f0ed8cf8506ede68c18e1c7
TatuWorkLenovo\Tatu <tatu@vaadin.com>
2025-11-05 09:46:05 +0200

    Add config class

---
+++ selection-grid-flow-demo/src/main/java/com/vaadin/componentfactory/selectiongrid/Config.java
@@ -0,0 +1,10 @@
+package com.vaadin.componentfactory.selectiongrid;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@StyleSheet(Lumo.STYLESHEET)
+public class Config implements AppShellConfigurator {
+    
+}
